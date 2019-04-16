4d60448867520669be9bc2dbdf52f6c930b6ee3d
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-16 14:22:53 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,10 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
-  happy_birthday(birthday_kids)
-    birthday_kids.each do |kids_name, age|
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    end
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
